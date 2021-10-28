a995f05beb817c409facbef8f86a15826560abc4
ZhannaKarpova <92864174+ZhannaKarpova@users.noreply.github.com>
2021-10-28 19:36:37 +0700

    Добавьте файлы проекта.

---
+++ lab_6(2)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_6_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово:");
+            string str = Console.ReadLine();
+            char[] newstr = str.ToCharArray();
+
+            Array.Reverse(newstr);
+
+            string str2 = new string(newstr);
+
+            if (str.Equals(str2))
+                Console.WriteLine("yes");
+            else Console.WriteLine("no");
+
+            Console.ReadLine();
+        }
+    }
+}
